5a952858df671e4b3352061e8ce7109bc55e1e6c
Kseniia Pivtorak <117482774+Ewwangelina@users.noreply.github.com>
2023-03-02 19:00:02 +0200

    add file

--- dz23.js
+++ dz23.js
@@ -8,35 +8,57 @@ class Student {
      * @type {[]}
      */
     grades = [];
+    /**
+     * @type {[]}
+     */
     attendance = [];
-    count = {present: 0, absent: 0}; // счетчик для посещений
-
+    /**
+     * счетчик для посещений
+     * @type {{absent: number, present: number}}
+     */
+    count = {present: 0, absent: 0};
 
     constructor() {
         this.generateGrades();
         this.generateAttendance();
     }
-    // выводим возраст по году рождения
+    /**
+     * выводим возраст по году рождения
+     * @returns {number}
+     */
     getAge(){
         let currentYear = new Date().getFullYear()
         return currentYear - this.yearBirth;
     }
-    // считаем рандомный вывод оценок
+    /**
+     * считаем рандомный вывод оценок
+     */
     generateGrades(){
         for(let i = 0; i <= 10; i++) {
             this.grades[i] = this.getRandom(70, 100);
         }
     }
+    /**
+     * округление оценок
+     * @param min
+     * @param max
+     * @returns {number}
+     */
     getRandom(min, max) {
         return Math.round(Math.random() * (max - min) + min);
     }
-    // заполняет пустой массив
+    /**
+     * заполняет пустой массив
+     */
     generateAttendance(){
         for(let i = 0; i < 25; i++) {
             this.attendance[i] = undefined;
         }
     }
-    // проверка ключей посещения-отсутсвия
+    /**
+     * проверка ключей посещения-отсутсвия
+     * @param value
+     */
     addToAttendance(value){
         let key = this.getEmptyKey();
         if(key) {
@@ -45,16 +67,27 @@ class Student {
             this.attendance.push(value);
         }
     }
+    /**
+     * провепка посещений
+     * @returns {Student}
+     */
     present(){
         this.addToAttendance(true);
         this.count.present++;
         return this;
     }
+    /**
+     * проверка пропусков
+     * @returns {Student}
+     */
     absent(){
         this.addToAttendance(false);
         this.count.absent++;
         return this;
     }
+    /**
+     * @returns {string}
+     */
     getEmptyKey(){
         for(let i in this.attendance) {
             if(!this.attendance[i]){
@@ -62,10 +95,18 @@ class Student {
             }
         }
     }
+
+    /**
+     * @param value
+     * @returns {number}
+     */
     getArrayAverage(value){
         return value.reduce((a, b) => (a + b)) / value.length;
     }
-    // возврат сообщнеий по суммарному значению баллов и посещений
+    /**
+     * возврат сообщнеий по суммарному значению баллов и посещений
+     * @returns {string}
+     */
     summary(){
         let aGrade = this.getArrayAverage(this.grades);
         let aPresent = this.count.present / this.attendance.length;
@@ -78,25 +119,45 @@ class Student {
             return 'Редиска!';
         }
     }
+
+    /**
+     * @param value
+     * @returns {Student}
+     */
     setFirstName(value) {
         this.firstName = value;
         return this;
     }
+
+    /**
+     *
+     * @param value
+     * @returns {Student}
+     */
     setLastName(value){
         this.lastName = value;
         return this;
     }
+
+    /**
+     * @param value
+     * @returns {Student}
+     */
     setYearBirth(value){
         this.yearBirth = value;
         return this;
     }
+
+    /**
+     * форма для вывода информации в консоль
+     */
     printInfo() {
        console.log(
-           'Фамилия: ' + this.firstName + '\n\r'
-           + 'Имя: ' + this.lastName + '\n\r'
-           + 'Возраст: ' + this.getAge() + '\n\r'
-           + 'Итог: ' + this.summary() + '\n\r'
-           + '------------------------------'
+           'Фамилия: ' + this.firstName + '\n' +
+           'Имя: ' + this.lastName + '\n' +
+           'Возраст: ' + this.getAge() + '\n' +
+           'Итог: ' + this.summary() + '\n' +
+           '------------------------------'
        );
     }
 }
@@ -106,7 +167,7 @@ new Student()
     .setLastName('Petrov')
     .setYearBirth(2007)
     .absent().absent().absent()
-    .present().present().present().present().present()
+    .present().present().present().present().present().present().present()
     .printInfo()
 
 new Student()
@@ -121,6 +182,6 @@ new Student()
     .setFirstName('Petr')
     .setLastName('Kovalenko')
     .setYearBirth(1994)
-    .absent()
-    .present().present().present().present()
+    .absent().absent()
+    .present().present().present().present().present().present()
     .printInfo()
